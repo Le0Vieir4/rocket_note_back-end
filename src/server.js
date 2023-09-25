c0dfec16a22252ba814c33a4d34df39b9b0aaa16
Leo <lvm9508@gmail.com>
2023-09-25 18:23:29 -0300

    enviroment ambient created

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 require("express-async-errors")
+require("dotenv/config")
 
 const cors = require("cors")
 const uploadConfig = require("./config/upload")
@@ -34,7 +35,7 @@ app.use((error, request, response, next) => {
 })
 
 
-PORT = 3000
+PORT = process.env.PORT || 3333
 
 
 app.listen(PORT,() => console.log(`server is running on Port ${PORT}`))
